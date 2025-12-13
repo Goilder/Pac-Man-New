d3cafc6b03f1485f3a9bb3b03fd8ab6b18ee833b
Ilya <iliypurtov@gmail.com>
2025-12-13 20:12:17 +0300

    Реализовал задачу: 5, 6,7, 8, 9, 10, 13, 14

---
+++ game/game.js
@@ -0,0 +1,12 @@
+
+function togglePause(){}
+function restartGame(){}
+function promptSave(){}
+function promptLoad(){}
+function newGame(){}
+
+function handleSlot(slot){}
+
+function resetSlot(){}
+function closeModal(){}
+function goBack(){}
