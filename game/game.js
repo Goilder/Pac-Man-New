f11f9736a6a403d93532ffd1ee8d22a16802a957
Goilder <iliypurtov@gmail.com>
2025-12-19 20:15:10 +0300

    Класс с призраками

--- game/game.js
+++ game/game.js
@@ -4,6 +4,26 @@ let currentMode = null // "save" или "load"
 
 let isPaused = false
 
+
+    class Ghost {
+        constructor(className, startIndex, speed) {
+            this.className = className
+            this.startIndex = startIndex
+            this.speed = speed
+            this.currentIndex = startIndex
+            this.isScared = false
+            this.timerId = NaN
+
+        }
+    }
+
+    const ghosts = [
+        new Ghost("blinky", 348, 250),
+        new Ghost("pinky", 376, 400),
+        new Ghost("inky", 351, 300),
+        new Ghost("clyde", 379, 500),
+    ]
+
     function createDefaultGameState() {
         const ghostStates = [
             { className: "blinky", currentIndex: 348, isScared: false },
