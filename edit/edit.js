74a1d04aa730ff7d82d0488b1a39e6b8b1c4e4a1
Goilder <iliypurtov@gmail.com>
2025-12-19 20:58:59 +0300

    Добавил экспорт карты в json файл и сохранение

--- edit/edit.js
+++ edit/edit.js
@@ -1,3 +1,19 @@
-function saveMap(){}
-function generate(){}
-function exportAsJSON(){}
+const size = 28;
+const grid = document.getElementById("map");
+const tileType = document.getElementById("tileType");
+let layout = Array(size * size).fill(0); // по дефорту все пустое
+
+
+function saveMap() {
+  document.getElementById("output").value = JSON.stringify(layout);
+}
+
+function generate() {
+  layout = generateRandomMap(size);
+  drawGrid();
+}
+
+function exportAsJSON() {
+  const content = JSON.stringify(layout, null, 2);
+  downloadFile(content, "map.json", "application/json");
+}
