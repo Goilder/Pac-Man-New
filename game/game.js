0e8ba7325b101821d0b5ad2b63c4ab9608417d95
Goilder <iliypurtov@gmail.com>
2025-12-19 20:22:48 +0300

    Функционал для пакмена

--- game/game.js
+++ game/game.js
@@ -295,8 +295,67 @@ import {maps}  from '../maps/map.js';
         checkForWin()
         // saveGameState()
     }
-
+    
     document.addEventListener("keyup", movePacman)
+    
+    function pacDotEaten() {
+        if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+            score++
+            scoreDisplay.innerHTML = score
+            squares[pacmanCurrentIndex].classList.remove("pac-dot")
+        }
+    }
+
+    function powerPelletEaten() {
+        if (squares[pacmanCurrentIndex].classList.contains("power-pellet")) {
+            score += 10
+            scoreDisplay.innerHTML = score
+            ghosts.forEach(ghost => ghost.isScared = true)
+            setTimeout(unScareGhosts, 10000)
+            squares[pacmanCurrentIndex].classList.remove("power-pellet")
+        }
+    }
+
+    function unScareGhosts() {
+        ghosts.forEach(ghost => ghost.isScared = false)
+    }
+
+    ghosts.forEach(ghost => squares[ghost.currentIndex].classList.add(ghost.className, "ghost"))
+
+        ghosts.forEach(ghost => moveGhost(ghost))
+
+    function moveGhost(ghost) {
+        const directions = [-1, 1, width, -width]
+        let direction = directions[Math.floor(Math.random() * directions.length)]
+
+        ghost.timerId = setInterval(function () {
+            //if next square your ghost is going to go to does not have a ghost and does not have a wall
+            if (
+                !squares[ghost.currentIndex + direction].classList.contains("ghost") &&
+                !squares[ghost.currentIndex + direction].classList.contains("wall")
+            ) {
+                squares[ghost.currentIndex].classList.remove(ghost.className, "ghost", "scared-ghost")
+                ghost.currentIndex += direction
+                squares[ghost.currentIndex].classList.add(ghost.className, "ghost")
+                // else find a new random direction to go in
+            } else direction = directions[Math.floor(Math.random() * directions.length)]
+            // if the ghost is currently scared
+            if (ghost.isScared) {
+                squares[ghost.currentIndex].classList.add("scared-ghost")
+            }
+
+            //if the ghost is currently scared and pacman is on it
+            if (ghost.isScared && squares[ghost.currentIndex].classList.contains("pac-man")) {
+                ghost.isScared = false
+                squares[ghost.currentIndex].classList.remove(ghost.className, "ghost", "scared-ghost")
+                ghost.currentIndex = ghost.startIndex
+                score += 100
+                scoreDisplay.innerHTML = score
+                squares[ghost.currentIndex].classList.add(ghost.className, "ghost")
+            }
+            checkForGameOver()
+        }, ghost.speed)
+    }
 
     function closeModal() {
         document.getElementById("save-load-modal").classList.add("hidden")
