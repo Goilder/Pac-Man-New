03159dd51f12c339031f0e07c64c00aa8848b07d
Goilder <iliypurtov@gmail.com>
2025-12-19 20:20:37 +0300

    Передвиженеи игрока

--- game/game.js
+++ game/game.js
@@ -234,6 +234,70 @@ import {maps}  from '../maps/map.js';
         squares[pacmanCurrentIndex].classList.add("pac-man");
     }
 
+        function movePacman(e) {
+        squares[pacmanCurrentIndex].classList.remove("pac-man")
+        // switch (e.keyCode) { deprecated
+        switch (e.key) {
+            // case 37:
+            case "ArrowLeft":
+                if (
+                    pacmanCurrentIndex % width !== 0 &&
+                    !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
+                    !squares[pacmanCurrentIndex - 1].classList.contains("ghost-lair")
+                ) {
+                    pacmanCurrentIndex -= 1
+                }
+                if ((pacmanCurrentIndex - 1) === 363) {
+                    pacmanCurrentIndex = 391
+                }
+                break
+            case "ArrowUp":
+                // case 38:
+                if (
+                    pacmanCurrentIndex - width >= 0 &&
+                    !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
+                    !squares[pacmanCurrentIndex - width].classList.contains("ghost-lair")
+
+                ) {
+                    pacmanCurrentIndex -= width
+                }
+                break
+            case "ArrowRight":
+                // case 39:
+                if (
+                    pacmanCurrentIndex % width < width - 1 &&
+                    !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
+                    !squares[pacmanCurrentIndex + 1].classList.contains("ghost-lair")
+                ) {
+                    pacmanCurrentIndex += 1
+                }
+                if (
+                    (pacmanCurrentIndex + 1) === 392
+                ) {
+                    pacmanCurrentIndex = 364
+                }
+                break
+            case "ArrowDown":
+                // case 40:
+                if (
+                    pacmanCurrentIndex + width < width * width &&
+                    !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
+                    !squares[pacmanCurrentIndex + width].classList.contains("ghost-lair")
+                ) {
+                    pacmanCurrentIndex += width
+                }
+                break
+        }
+        squares[pacmanCurrentIndex].classList.add("pac-man")
+        pacDotEaten()
+        powerPelletEaten()
+        checkForGameOver()
+        checkForWin()
+        // saveGameState()
+    }
+
+    document.addEventListener("keyup", movePacman)
+
     function closeModal() {
         document.getElementById("save-load-modal").classList.add("hidden")
         currentMode = null
